509c12cab47dcee712d4f4e98c71bab746757ae0
Kemil WOO <Kemil.penapolanco26@compscihigh.org>
2026-04-15 16:46:09 +0000

    Teie

---
+++ views/teacherScript.js
@@ -0,0 +1,18 @@
+// Add an event listener to the form that upon submits creates a new teacher
+// When the teacher is created redirect to the teachers page
+const createForm = document.querySelector("form");
+createForm.addEventListener("submit",async (e) =>{
+    e.preventDefault();
+    const teacherData = new FormData(createForm);
+    const reqBody=Object.fromEntries(teacherData);
+    await fetch("/add",{
+        method:"POST",
+        header:{
+            "Content-Type":"application/json",
+        },
+        body:JSON.stringify(reqBody),
+    });
+    window.location.href="/";
+        }
+     })
+})
